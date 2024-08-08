443aa03c288be540c589110b6e47aba1c98aea59
Jordan Torres <50530529+jortorres@users.noreply.github.com>
2024-08-08 15:40:59 +0000

    New Practice problems

---
+++ ListMethods.py
@@ -0,0 +1,5 @@
+numbers = [1,2,3,4,5]
+
+numbers.insert(0,-1)
+numbers.remove(3)
+print(len(numbers))
---
+++ Lists.py
@@ -0,0 +1,6 @@
+names = ["Katie", "Paul", "Jordan", "Jaiden", "Jazmine"]
+
+
+names[0] = 'Grams'
+print(names[0:3])
+print(names)
