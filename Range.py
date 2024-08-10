de1df01d81ea97ecb6ba811a68d546c4918fcacf
jortorres <jordan.torres1@gmail.colm>
2024-08-10 08:01:51 -0500

    learning range and tuples

---
+++ Range.py
@@ -0,0 +1,6 @@
+step = 2
+start = 5
+stop = 100
+
+for number in range(start, stop, step):
+    print(number)
---
+++ Tuples.py
@@ -0,0 +1,6 @@
+numbers = (1, 2, 3, 3)
+numbers.count(3)
+              
+              
+# Use tuples if you don't want anyone to change your list.
+
